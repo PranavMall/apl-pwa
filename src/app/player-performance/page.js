3579752e775ee4fef7964d32496b27d2c7238ea8
Pranav <pranavsharma1930@gmail.com>
2025-01-30 15:13:35 +0400

    Update page.js

--- src/app/player-performance/page.js
+++ src/app/player-performance/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { db } from '../../../firebase';
 import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
