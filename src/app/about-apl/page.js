fef59feb048fde8e211ce6f926515d7041b21d27
Pranav <pranavsharma1930@gmail.com>
2025-01-30 19:06:41 +0400

    Update About Apl Page

---
+++ src/app/about-apl/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+// ... other imports
+
+const AboutAPLPage = () => {
+  // your component code
+  return (
+    // your JSX
+  );
+};
+
+export default AboutAPLPage;  // Make sure this export is present
