091b0271ab447849ed0140efcc616c6ea8ecd8ec
Pranav <pranavsharma1930@gmail.com>
2025-02-23 19:49:58 +0400

    Fix import writeBatch from firebase/firestore

--- src/app/services/cricketService.js
+++ src/app/services/cricketService.js
@@ -8,7 +8,8 @@ import {
   orderBy,
   limit,
   runTransaction,
-  where
+  where,
+  writeBatch  // Add this import
 } from 'firebase/firestore';
 import { PlayerService } from './playerService';
 import { PointService } from './pointService';
