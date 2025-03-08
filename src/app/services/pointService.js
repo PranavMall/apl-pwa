39dba2a8ddb8d52a6a5a24cabaac545f62e8725a
Pranav <pranavsharma1930@gmail.com>
2025-03-08 22:37:01 +0400

    Importing PlayerMasterServices

--- src/app/services/pointService.js
+++ src/app/services/pointService.js
@@ -3,6 +3,7 @@
 
 import { db } from '../../firebase';
 import { doc, setDoc, getDoc, collection, query, where, getDocs, runTransaction } from 'firebase/firestore';
+import { PlayerMasterService } from '@/app/services/playerMasterService';
 
 export class PointService {
   static POINTS = {
