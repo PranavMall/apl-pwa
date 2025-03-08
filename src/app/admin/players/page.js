95dc71e74d6d7a1f2db4ccde43e013ae831854f3
Pranav <pranavsharma1930@gmail.com>
2025-03-08 22:18:51 +0400

    Correction of firebase folder

--- src/app/admin/players/page.js
+++ src/app/admin/players/page.js
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
-import { db } from '../../../../firebase'; // Adjust path as needed for your project
+import { db } from '../../../firebase'; // Adjust path as needed for your project
 import { PlayerMasterService } from '@/app/services/PlayerMasterService';
 import { getPlayerList, mapPlayerIds } from '../player-management';
 
