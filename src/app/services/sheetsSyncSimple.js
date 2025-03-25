08d9fbf559d086daf852c161a9db5597e0309b22
Pranav <pranavsharma1930@gmail.com>
2025-03-25 18:56:17 +0400

    const weeklyStatsRef = doc(db, 'userWeeklyStats', `${userTeam.userId}_${tournament.id}_${weekNum}`);

--- src/app/services/sheetsSyncSimple.js
+++ src/app/services/sheetsSyncSimple.js
@@ -207,7 +207,7 @@ export class SheetsSyncService {
               }
               
               // Update weekly stats
-              const weeklyStatsRef = doc(db, 'userWeeklyStats', `${userTeam.userId}_${tournament.id}`);
+              const weeklyStatsRef = doc(db, 'userWeeklyStats', `${userTeam.userId}_${tournament.id}_${weekNum}`);
               const weeklyStatsDoc = await getDoc(weeklyStatsRef);
               
               if (weeklyStatsDoc.exists()) {
