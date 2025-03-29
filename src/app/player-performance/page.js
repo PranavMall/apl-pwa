e7f1df7c14a6f85f97864661e811e35728bda428
Pranav <pranavsharma1930@gmail.com>
2025-03-29 09:23:05 +0400

    Firebase path correction

--- src/app/player-performance/page.js
+++ src/app/player-performance/page.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/app/components/ui/card';
 import { collection, getDocs, query } from 'firebase/firestore';
-import { db } from '../../../firebase';
+import { db } from '../../firebase';
 import styles from './page.module.css';
 
 const PlayerPerformancePage = () => {
